3848b638781a21529ab3b356d200aac0b1f6b9a4
te9no <tatuys9428@gmail.com>
2025-05-31 16:16:32 +0900

    fix

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
         parts = [ "L rgbled_adapter" "R" ];
         snippets = [ "zmk-usb-logging" ];
         enableZmkStudio = true;
-        zephyrDepsHash = "sha256-0vLyjaO05FOiXaTC0YoNiWRnwdIrr4xen2e1p/Qi0QQ=";
+        zephyrDepsHash = "sha256-Qh7/i4O1PHfD0geyZFWQLPxi8Z71Lb/hVJGf/IZlQPM=";
         meta = {
           description = "ZMK firmware";
           license = nixpkgs.lib.licenses.mit;
